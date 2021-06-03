3bf699baaa91b125dc53a206cb586439d13c7459
Shankar Isaac <khaarn.s@gmail.com>
2021-06-03 16:30:06 +1000

    finished rails watch list functionality

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+    @movies = Movie.all
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    # @list = List.find(params[:list_id])
+    
+    redirect_to list_path(@bookmark.list)
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
